c76cec0873bb174f01afb70842d3f6dc3dd7ace7
Ryan Lewis <ryanlewis@Ryans-Mac.local>
2018-12-11 15:53:38 -0800

    Added in Xcode generated code

---
+++ Final Project/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Final Project
+//
+//  Created by Ryan Lewis on 12/11/18.
+//  Copyright © 2018 Ryan Lewis. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
